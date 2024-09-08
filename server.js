4d7e6e332fe64d4f2141b2be97d797ac6fe230f7
Munib07 <munib.bit07@gmail.com>
2024-09-08 17:23:40 +0530

    Fix in AI prompt

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ connectDB();
 //rest object
 const app = express();
 
+//set cors
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 //sample endpoint
 app.get("/api", (req, res) => {
   // Send a response to the client
